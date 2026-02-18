2dc233c1a9bff19dff919686d7718af3f9c740ad
JUANc798 <juan.baracaldo@upb.edu.co>
2026-02-18 09:41:15 -0500

    Archivos 1 y 2 del repositorio
    Archivos 1 y 2 del repositorio

---
+++ 2.CondicionalesSimples/2.CondicionalesSimples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.CondicionalesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos.*/
+            /*string nombre = "";
+            float sueldo = 0;
+            Console.WriteLine("Ingrese el nombre del usuario");
+            nombre=Console.ReadLine();//Todo lo que el usuario ingresa desde la consola llega como una cadena de caracteres
+            Console.WriteLine("Ingrese el valor del sueldo");
+            sueldo=Convert.ToSingle( Console.ReadLine() );
+
+            if (sueldo>=3000)
+            {
+                Console.WriteLine($"El usuario {nombre}, debe abonar impuestos");
+            }*/
+
+            /*2.	Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”.  */
+
+
+        }
+    }
+}
